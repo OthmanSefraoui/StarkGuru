c6f274c1f69d5ff85f21296f5acdc7cfb69c582f
Ümit Gül <uemitgul@gmail.com>
2022-04-25 20:58:10 +0200

    priceA&B

--- frontend/Components/swapTokens.tsx
+++ frontend/Components/swapTokens.tsx
@@ -1,40 +1,64 @@
 import { Tag, Avatar, Text, TagLabel, NumberInput, NumberInputField, HStack, Stack, Spacer } from '@chakra-ui/react'
 import TokenABalance from 'Components/tokenABalance';
+import TokenAPrice from './tokenAPrice';
 import TokenBBalance from 'Components/tokenBBalance';
 import { useState } from "react";
 import React, { useMemo } from 'react';
 import { useStarknet, useStarknetCall } from '@starknet-react/core';
 import { uint256ToBN } from 'starknet/dist/utils/uint256';
-import { useAMMContract, useTokenAContract } from '~/hooks/contracts';
+import { useAMMContract, useTokenAContract, useTokenBContract } from '~/hooks/contracts';
 
 const SwapTokens = () => {
 
     const [valueA, setValueA] = React.useState('');
 
+    //Price A
     const { account } = useStarknet();
     const { contract } = useAMMContract();
     const tokenAAddress = useTokenAContract().contract?.address;
 
     console.log(tokenAAddress);
 
-    const { data, loading, error } = useStarknetCall({
+    const resp1 = useStarknetCall({
         contract,
         method: 'get_price',
         args: [1, tokenAAddress],
     });
 
-    const content = useMemo(() => {
-        if (loading || !data?.length) {
+    const priceA = useMemo(() => {
+        if (resp1.loading || !resp1.data?.length) {
             return <div>Loading price</div>;
         }
 
-        if (error) {
-            return <div>Error: {error}</div>;
+        if (resp1.error) {
+            return <div>Error: {resp1.error}</div>;
         }
 
-        const price = data[0] / 1000000000000000000;
+        const price = resp1.data[0] / 1000000000000000000;
         return price;
-    }, [data, loading, error]);
+    }, [resp1.data, resp1.loading, resp1.error]);
+
+    //Price B
+    const tokenAddressB = useTokenBContract().contract?.address;
+    const resp2 = useStarknetCall({
+        contract,
+        method: 'get_price',
+        args: [1, tokenAddressB],
+    });
+
+    const priceB = useMemo(() => {
+        if (resp2.loading || !resp2.data?.length) {
+            return <div>Loading price</div>;
+        }
+
+        if (resp2.error) {
+            return <div>Error: {resp1.error}</div>;
+        }
+
+        const price = resp2.data[0] / 1000000000000000000;
+        return price;
+    }, [resp2.data, resp2.loading, resp2.error]);
+
 
     return (
         <Stack>
@@ -60,7 +84,7 @@ const SwapTokens = () => {
             </HStack>
             <Text>You buy</Text>
             <HStack>
-                <Text size='xl' width={400}>{valueA * content}</Text>
+                <Text size='xl' width={400}>{(valueA*priceA)/priceB}</Text>
                 
                 <Tag size='lg' colorScheme='teal' borderRadius='full'>
                     <Avatar
