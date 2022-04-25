fb3d25f5accc7e37b19cbeb0c6153ed7bd56e711
Alexandre <alexandrewlff@gmail.com>
2022-04-26 00:50:13 +0200

    Backend get orders & execute them

--- backend/index.ts
+++ backend/index.ts
@@ -3,38 +3,30 @@ import fs from 'fs';
 import { CronJob } from 'cron';
 import Starknet from './lib/Starknet';
 
-import accountContract from './accountContract';
+// const keyPair = JSON.parse(
+//   fs.readFileSync('./key-pair.json').toString('ascii'),
+// );
 
-const keyPair = JSON.parse(
-  fs.readFileSync('./key-pair.json').toString('ascii'),
-);
+const starknet = new Starknet('https://hackathon-3.starknet.io');
 
-const starknet = new Starknet(
-  'https://hackathon-3.starknet.io',
-  accountContract,
-  keyPair,
-);
-
-const init = async () => {
-  await starknet.collectOrders();
-};
+let collecting = false;
 
 const job = new CronJob(
-  '* * * * * *',
-  () => {
-    console.log('Job is running');
-    starknet.orders.forEach(async (order) => {
-      await starknet.checkPrice(order);
-    });
+  '*/2 * * * * *',
+  async () => {
+    if (collecting) return;
+    collecting = true;
+    try {
+      // await starknet.checkStatuses();
+      await starknet.collectOrders();
+      await starknet.checkPrices();
+    } catch (err) {
+      console.log(err);
+    }
+    collecting = false;
   },
   null,
   true,
 );
 
-init();
 job.start();
-
-// starknet.getPrice(
-//   1,
-//   '1024954592621139756096955849939395900823875067926016197778070902863251962652',
-// );
