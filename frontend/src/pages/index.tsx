2b5a5d7b4ff873273cb3fbfef59660ecc93d92d1
Ümit Gül <uemitgul@gmail.com>
2022-04-25 14:46:50 +0200

    chose token

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,7 +6,7 @@ import { ConnectWallet } from '~/components/ConnectWallet'
 import { IncrementCounter } from '~/components/IncrementCounter'
 import { TransactionList } from '~/components/TransactionList'
 import { useCounterContract } from '~/hooks/counter'
-import { Container, Heading, Text, Button, NumberInput, NumberInputField } from '@chakra-ui/react'
+import { Container, Heading, Text, Button, NumberInput, NumberInputField, HStack, Select } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
   const { contract: counter } = useCounterContract()
@@ -31,12 +31,26 @@ const Home: NextPage = () => {
       </Heading>
       <ConnectWallet />
       <Heading>Swap</Heading>
-      <NumberInput defaultValue={0.0}>
-        <NumberInputField />
-      </NumberInput>
-      <NumberInput defaultValue={0.0}>
-        <NumberInputField />
-      </NumberInput>
+      <HStack>
+        <NumberInput defaultValue={0.0}>
+          <NumberInputField />
+        </NumberInput>
+        <Select placeholder='Select option'>
+          <option value='option1'>ETH</option>
+          <option value='option2'>DAI</option>
+          <option value='option3'>USDC</option>
+        </Select>
+      </HStack>
+      <HStack>
+        <NumberInput defaultValue={0.0}>
+          <NumberInputField />
+        </NumberInput>
+        <Select placeholder='Select option'>
+          <option value='option1'>ETH</option>
+          <option value='option2'>DAI</option>
+          <option value='option3'>USDC</option>
+        </Select>
+      </HStack>
 
       <Button colorScheme='teal' size='md'>
         Swap Tokens
